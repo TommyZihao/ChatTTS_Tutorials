9db7fd3c5225ea6c2099868b8cacbc7ac594803c
Tommy in Tongji <36354458+TommyZihao@users.noreply.github.com>
2024-06-14 16:31:11 +0800

    Update zihao_tts.py

--- zihao_chattts_20240613_4/zihao_tts.py
+++ zihao_chattts_20240613_4/zihao_tts.py
@@ -9,7 +9,12 @@ import soundfile
 from IPython.display import Audio
 
 chat = ChatTTS.Chat()
-chat.load_models(compile=False) # 设置为Flase获得更快速度，设置为True获得更佳效果
+
+# 加载默认下载的模型
+# chat.load_models(compile=False) # 设置为Flase获得更快速度，设置为True获得更佳效果
+
+# 趋动云：加载本地下载好的模型
+chat.load_models(compile=False, source='local', local_path='/gemini/pretrain') # 设置为Flase获得更快速度，设置为True获得更佳效果
 
 # 使用随机音色
 # speaker = chat.sample_random_speaker()
